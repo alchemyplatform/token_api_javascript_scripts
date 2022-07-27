117c6c3d437e06b6d37464d6bb99c5def456775a
Aniket Gupta <aniketguptaknp@gmail.com>
2022-07-27 16:12:04 +0530

    Use the new SDK format for token balance fetch
    Use the new SDK format

--- token-balances-from-alchemyWeb3.js
+++ token-balances-from-alchemyWeb3.js
@@ -1,23 +1,22 @@
-import { createAlchemyWeb3 } from "@alch/alchemy-web3";
- 
-//Replace with your Alchemy API Key:
-const apiKey = "demo";
- 
-// Initialize an alchemy-web3 instance:
-const web3 = createAlchemyWeb3(
-   `https://eth-mainnet.alchemyapi.io/v2/${apiKey}`,
- );
- 
-// Replace with the wallet address you want to query:
+// Setup: npm install alchemy-sdk
+import { Alchemy, Network } from "alchemy-sdk";
+
+const config = {
+  apiKey: "<-- ALCHEMY APP API KEY -->",
+  network: Network.ETH_MAINNET,
+};
+const alchemy = new Alchemy(config);
+
+//Feel free to switch this wallet address with another address
 const ownerAddress = "0x00000000219ab540356cbb839cbe05303d7705fa";
-/* 
-Replace with the token contract address you want to query:
-The below address Corresponds to USDT
-*/
+
+//The below token contract address corresponds to USDT
 const tokenContractAddresses = ["0xdAC17F958D2ee523a2206206994597C13D831ec7"];
 
-/*
-** Fetching the token Balance with Alchemy's getTokenBalances API
-*/
-const data = await web3.alchemy.getTokenBalances( ownerAddress, tokenContractAddresses);
-console.log("Response Object for getTokenBalances\n", data)
+const data = await alchemy.core.getTokenBalances(
+  ownerAddress,
+  tokenContractAddresses
+);
+
+console.log("Token balance for Address");
+console.log(data);
